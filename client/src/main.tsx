bd655da0c17fc35de95aa22de12e6c77a8cd6617
fortune <fortunatusmicheal@gmail.com>
2025-06-04 16:04:27 +0100

    Update Wallet Connect

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 createRoot(document.getElementById('root')!).render(
-  <TonConnectUIProvider manifestUrl='https://jade-imperial-hawk-534.mypinata.cloud/ipfs/bafybeigkixu527m6rtupe3yimytsedkhroqve4bucaru6rrvkzw3ptcle4/'>
+  <TonConnectUIProvider manifestUrl='https://jade-imperial-hawk-534.mypinata.cloud/ipfs/bafybeigkixu527m6rtupe3yimytsedkhroqve4bucaru6rrvkzw3ptcle4/manifest.json'>
     <StrictMode>
       <App />
     </StrictMode>
